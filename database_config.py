a25251e5da43c829612c5abb6e912bb99cfc666c
Kautsar Fadly Firdaus <kautsarfadlyfirdaus@gmail.com>
2020-02-03 08:52:19 +0700

    fix uploaded file

---
+++ database_config.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+# invite your bot to the server
+# https://discordapp.com/oauth2/authorize?client_id=YOUR_CLIENT_ID&scope=bot&permissions=YOUR_PERMISSION
+
+# app init main
+YOUR_DISCORD_TOKEN = "YOUR_DISCORD_TOKEN"
+YOUR_CLIENT_BOT_ID = YOUR_CLIENT_BOT_ID
+YOUR_USER_ID = YOUR_USER_ID
+is_logging = False
+
+# TODOIST SETTING
+YOUR_TODOIST_TOKEN = "YOUR_TODOIST_TOKEN"
+
+
+
